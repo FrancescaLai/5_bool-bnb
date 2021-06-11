d6feb6ee18ed73167ba837ce4f4806402fb7300d
Tommaso <dossena.tommaso.dt@gmail.com>
2021-06-11 17:49:01 +0200

    Add: new users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,17 +13,41 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $newUser = new User();
+        $user_1 = new User();
 
-        $newUser->email = 'franco@gmail.com';
-        $newUser->password = Hash::make('123abc456');
-        $newUser->name = 'Franco';
-        $newUser->surname = 'Rossi';
-        $newUser->birth_date = '1995-06-23';
-        $newUser->phone_number = '3583456789';
-        $newUser->document_type = 'id';
-        $newUser->document_number = ' CA12345AA';
+        $user_1->email = 'samuele.madrigali@gmail.com';
+        $user_1->password = Hash::make('12345678');
+        $user_1->name = 'Samuele';
+        $user_1->surname = 'Madrigali';
+        $user_1->birth_date = '1990-06-23';
+        $user_1->phone_number = '3583456789';
+        $user_1->document_type = 'id';
+        $user_1->document_number = ' MR47929AA';
+        $user_1->save();
 
-        $newUser->save();
+        $user_2 = new User();
+
+        $user_2->email = 'amedeo.pisani@gmail.com';
+        $user_2->password = Hash::make('87654321');
+        $user_2->name = 'Amedeo';
+        $user_2->surname = 'Pisani';
+        $user_2->birth_date = '1971-12-11';
+        $user_2->phone_number = '3436459782';
+        $user_2->document_type = 'id';
+        $user_2->document_number = 'QK56905AA';
+        $user_2->save();
+
+
+        $user_3 = new User();
+
+        $user_3->email = 'viola.marino@gmail.com';
+        $user_3->password = Hash::make('87654321');
+        $user_3->name = 'Viola';
+        $user_3->surname = 'Marino';
+        $user_3->birth_date = '1994-04-22';
+        $user_3->phone_number = '3486552723';
+        $user_3->document_type = 'id';
+        $user_3->document_number = 'MV24801AA';
+        $user_3->save();
     }
 }
