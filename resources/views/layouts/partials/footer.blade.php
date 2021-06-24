1b4089b0f6b63be0737011ed76cc5ef589832322
FrancescaLai <francescalai1690@gmail.com>
2021-06-24 17:56:29 +0200

    Fix: footer + footer responsive

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,60 +1,35 @@
 <footer>
   <div class="container">
     <div class="row">
-      <div class="col-xl-4 col-lg-3 col-sm-6 column"><span>boolbnb</span></div>
+      <div class="col-xl-4 col-lg-12 col-sm-12">
+        <a class="logo">
+          <h3>boolbnb</h3>
+        </a>
+      </div>
 
-        <div class="col-xl-2 col-lg-3 col-sm-3 column">
-          <div class="row">
-            <a href="{{route('guest.search')}}">Search</a>
-          </div>
-          {{-- <div class="row">
-            <a href="">Projects</a>
-          </div> --}}
+        <div class="col-xl-2 col-lg-3 col-sm-12">
+          <a href="{{route('guest.search')}}">Search</a>
         </div>
 
-        <div class="col-xl-2 col-lg-3 col-sm-3 column">
-          <div class="row">
-            <a href="{{route('admin.index')}}">Dashboard</a>
-          </div>
-          {{-- <div class="row">
-            <a href="">Press</a>
-          </div> --}}
+        <div class="col-xl-2 col-lg-3 col-sm-12">
+          <a href="{{route('admin.index')}}">Dashboard</a>   
         </div>
 
-        <div class="col-xl-2 col-lg-3 d-sm-none d-md-none d-lg-block column">
-          <div class="row">
-            <a href="#">About</a>
-          </div>
-          {{-- <div class="row">
-            <a href="">Download</a>
-          </div> --}}
+        <div class="col-xl-2 col-lg-3 col-sm-12">
+          <a href="#">About</a>
         </div>
 
-        <div class="col-lg-9 d-none d-lg-block d-xl-none column">
-        </div>
-
-
-        <div class="col-xl-2 col-lg-3 col-sm-9 column">
-          <div class="row">
-            <a href="{{route('register')}}" class="joinCommunity">JOIN OUR COMMUNITY &#128293</a>
-          </div>
-          {{-- <div class="row email d-flex align-items-center">
-            <input type="email" class="form-control" placeholder="Enter your email">
-            <div>
-              <a href=""><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M12.3464 7.50716C12.6186 7.19494 13.0924 7.16249 13.4046 7.43468L17.9929 11.4347C18.1562 11.5771 18.25 11.7833 18.25 12C18.25 12.2168 18.1562 12.4229 17.9929 12.5653L13.4046 16.5653C13.0924 16.8375 12.6186 16.8051 12.3464 16.4929C12.0742 16.1806 12.1067 15.7069 12.4189 15.4347L15.4984 12.75H6.5C6.08579 12.75 5.75 12.4142 5.75 12C5.75 11.5858 6.08579 11.25 6.5 11.25H15.4984L12.4189 8.56534C12.1067 8.29315 12.0742 7.81938 12.3464 7.50716Z" fill="#ffffff"/>
-                </svg></a>
-            </div> --}}
-          </div>
-        </div>
-
-        <div class="col-sm-3 d-none d-sm-block d-md-block d-lg-none column">
-          <div class="row">
+        <div class="col-sm-3 d-none d-sm-block d-md-block d-lg-none">
+          <div>
             <a href="">Destinazioni</a>
           </div>
-          <div class="row">
+          <div>
             <a href="">Download</a>
           </div>
+        </div>
+
+        <div class="col-xl-2 col-lg-3 col-sm-12">
+          <a href="{{route('register')}}" class="joinCommunity">JOIN OUR COMMUNITY &#128293</a>
         </div>
 
     </div>
